33b054661142aa1d543a57b53f6828b5eda1717c
Ricardo Goto <ricgoto@gmail.com>
2011-05-12 15:48:41 -0400

    Removing broken authentication event handler

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,8 +10,10 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
-import spl.*
-import org.hibernate.StaleObjectStateException
+//import spl.*
+//import org.springframework.orm.hibernate3.HibernateOptimisticLockingFailureException
+//import org.hibernate.StaleObjectStateException
+//import org.springframework.dao.OptimisticLockingFailureException
 import grails.plugins.springsecurity.SecurityConfigType
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
@@ -100,17 +102,19 @@ grails.plugins.springsecurity.useSecurityEventListener = true
 grails.plugins.springsecurity.userLookup.userDomainClassName = 'spl.AuthUser'
 grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'spl.AuthUserAuthRole'
 grails.plugins.springsecurity.authority.className = 'spl.AuthRole'
-grails.plugins.springsecurity.onInteractiveAuthenticationSuccessEvent = { e, appCtx ->
-	User.withTransaction {
-		def user = appCtx.springSecurityService.currentUser
-		try {
-			user.lastLogin = new Date()
-			user.save(flush: true)
-		} catch(StaleObjectStateException ex) {
-			user = user.merge()
-		}
-	}
-}
+//grails.plugins.springsecurity.onInteractiveAuthenticationSuccessEvent = { e, appCtx ->
+//	User.withTransaction {
+//		def user = User.get(appCtx.springSecurityService.currentUser.id)
+//		try {
+//			user.lastLogin = new Date()
+//			println "DEBUG: ${user.lastLogin} trying to save user: ${user.username}"
+//			user.save()
+//		} catch(HibernateOptimisticLockingFailureException  ex) {
+//			user = user.merge()
+//			println "DEBUG: ${user.lastLogin} caught exception, merging user: ${user.username}"
+//		}
+//	}
+//}
 
 // Mail configs
 grails.mail.default.from='contact@starplayersleague.com'
