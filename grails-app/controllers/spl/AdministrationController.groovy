19acdc94b4c5ba3e710dfee1f30b04d445b4ed59
Ricardo Goto <ricgoto@gmail.com>
2011-05-14 22:26:07 -0400

    Adding modify/reset match result admin actions

--- grails-app/controllers/spl/AdministrationController.groovy
+++ grails-app/controllers/spl/AdministrationController.groovy
@@ -8,5 +8,36 @@ class AdministrationController {
 		
 	}
 	
+	def modifyMatchResult = {
+		if (params.modifyMatchId) {
+			redirect(controller: 'profile', action: 'reportScore', id: params.modifyMatchId)
+		} else {
+			flash.message = "Please specify match ID to modify"
+			redirect(action: 'index')
+		}
+	}
+	
+	def resetMatchResult = {
+		def match = Match.get(params.resetMatchId)
+		if (params.resetMatchId) {
+			match.winner = null
+			match.loserScore = null
+			match.played = false
+			for (_game in match.games) {
+				_game.winner = null
+			}
+			match.save(flush: true)
+		
+			for (_entry in match.entries) {
+				_entry.updateMatchGameStats()
+				_entry.save(flush: true)
+			}
+			flash.message = "Reset result of match ID ${params.resetMatchId} successful"
+			redirect(action: 'index')
+		} else {
+			flash.message = "Please specify match ID to reset"
+			redirect(action: 'index')
+		}
+	}
 	
 }
--- grails-app/views/administration/index.gsp
+++ grails-app/views/administration/index.gsp
@@ -6,5 +6,30 @@
 	<body>
 		<h1>Administration</h1>
 		<div class="break"></div>
+		<g:if test="${flash.message}">
+            <div class="message">${flash.message}</div>
+        </g:if>
+		<g:form>
+			<div class="infoleft">
+				<p>Modify result of match ID: </p>
+			</div>
+			<div class="inforight">
+	    		<p>
+	    			<input type='text' class='text_' name='modifyMatchId'/>
+	    			<g:actionSubmit class="submitButton" action="modifyMatchResult" value="Update" />
+	    		</p>
+	    	</div>
+	    </g:form>
+	    <g:form>
+			<div class="infoleft">
+				<p>Reset result of match ID: </p>
+			</div>
+			<div class="inforight">
+	    		<p>
+	    			<input type='text' class='text_' name='resetMatchId'/>
+	    			<g:actionSubmit class="submitButton" action="resetMatchResult" value="Update" />
+	    		</p>
+	    	</div>
+	    </g:form>
 	</body>
 </html>
